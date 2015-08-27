86c39ff4957d7945d0fe591dec31c53acc7dbb7d
jbdi <anatoly@metadesign.ru>
2015-08-27 10:16:49 +0700

    При смене лимита, не сбрасывается GET

--- pure_pagination/views.py
+++ pure_pagination/views.py
@@ -1,6 +1,7 @@
 from datetime import datetime, timedelta
 
 from django.shortcuts import redirect
+from django.core.urlresolvers import reverse
 from django.conf import settings
 from pure_pagination.paginator import ITEMS_PER_PAGE_REDIRECT_VIEW_NAME, ITEMS_PER_PAGE_COOKIE_NAME
 
@@ -8,7 +9,8 @@ from pure_pagination.paginator import ITEMS_PER_PAGE_REDIRECT_VIEW_NAME, ITEMS_P
 def set_ipp_limit(request, limit):
     if not ITEMS_PER_PAGE_REDIRECT_VIEW_NAME:
         raise KeyError('Set ITEMS_PER_PAGE_REDIRECT_VIEW_NAME parameter')
-    response = redirect(ITEMS_PER_PAGE_REDIRECT_VIEW_NAME)
+    url_querystring = '%s?%s' % (reverse(ITEMS_PER_PAGE_REDIRECT_VIEW_NAME), request.META.get('QUERY_STRING'))
+    response = redirect(url_querystring)
     cookie_expires = datetime.now() + timedelta(365*10)
     response.set_cookie(ITEMS_PER_PAGE_COOKIE_NAME, value=limit, expires=cookie_expires, path='/', httponly=True)
     return response
